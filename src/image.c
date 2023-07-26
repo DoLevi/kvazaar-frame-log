ad6fda663ac39488fd3cff1f2a5c75e27f651d8e
dolevi-tub <dolevi@tu-berlin.de>
2023-07-26 09:06:26 +0200

    print to stderr

--- src/image.c
+++ src/image.c
@@ -170,8 +170,8 @@ kvz_picture *kvz_image_make_subimage(kvz_picture *const orig_image,
   assert((x_offset % 2) == 0);
   assert((y_offset % 2) == 0);
 
-  fprintf(stdout, "x_offset: %u + width: %u <= %u", x_offset, width, orig_image->width);
-  fprintf(stdout, "y_offset: %u + height: %u <= %u", y_offset, height, orig_image->height);
+  fprintf(stderr, "x_offset: %u + width: %u <= %u", x_offset, width, orig_image->width);
+  fprintf(stderr, "y_offset: %u + height: %u <= %u", y_offset, height, orig_image->height);
   assert(x_offset + width <= orig_image->width);
   assert(y_offset + height <= orig_image->height);
 
